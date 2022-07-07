20bd6bbc10ed0c3d26420afd505a55cf6b60d742
Jidnesha-kharkar <108565787+Jidnesha-kharkar@users.noreply.github.com>
2022-07-07 15:32:47 +0530

    Update musicplayer.py

--- musicplayer.py
+++ musicplayer.py
@@ -5,12 +5,12 @@ print("no. of avaiable songs\n1.rockabye\n2.godzilla\n3.gangster")
 order = input('enter the music which you want to play')
 
 if "rockabye" in order:
-    playsound('C:\\Users\\hp\\Downloads\\rockabye.mp3')
+    playsound('C:\\Users\\hp\\Downloads\\rockabye.mp3') #location where the audio file is saved.
 
 elif "godzilla" in order:
-    playsound('C:\\Users\\hp\\Downloads\\godzilla.mp3')
+    playsound('C:\\Users\\hp\\Downloads\\godzilla.mp3') #location where the audio file is saved.
 
 elif "gangster" in order:
-    playsound('C:\\Users\\hp\\Downloads\\gangster.mp3')
+    playsound('C:\\Users\\hp\\Downloads\\gangster.mp3') #location where the audio file is saved.
 
    
